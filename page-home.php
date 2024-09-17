f6b76d13000e594c47897b6f8ea1e4245f4af48a
Maxime Bérard <contact@maximeberard.com>
2024-09-17 12:03:32 +0200

    Add col percent less var // add categories / tags query on home page // add footer tag

--- page-home.php
+++ page-home.php
@@ -8,6 +8,9 @@ $context = Timber::context();
 $timber_post     = new Timber\Post();
 $context['post'] = $timber_post;
 
+// $context['categories'] = Timber::get_terms('category', array('hide_empty' => true));
+// $context['tags'] = Timber::get_terms('post_tag', array('hide_empty' => true));
+
 // Sample posts query
 // $argsPosts = array(
 //     'post_type' => 'post',
